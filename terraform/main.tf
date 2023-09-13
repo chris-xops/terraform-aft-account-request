3ba8c430a28857b9ee60851fdf603a04bd92075b
Christian Alonso <chris@swell.is>
2023-09-13 19:49:21 -0300

    adding cds2 account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "cds" {
 
   account_customizations_name = "Costumer Data Store Single Tenant Account"
 }
+
+module "cds2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "optimise-lattice-isle@xperiencops.com"
+    AccountName               = "cds2-aft"
+    ManagedOrganizationalUnit = "CDS"
+    SSOUserEmail              = "chris@xperiencops.com"
+    SSOUserFirstName          = "CDS1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Managed_by" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "XOPS DevOps"
+    change_reason       = "CDS single tenant account creation"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "Costumer Data Store Single Tenant Account"
+}
