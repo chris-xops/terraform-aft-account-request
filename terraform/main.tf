c39c9bf935ba82090b6f117eae544d18ec2e7eae
Christian Alonso <chris@swell.is>
2023-09-26 11:51:22 -0300

    new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,3 @@
-module "cds" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "aws-cmdb-sandbox@xops.io"
-    AccountName               = "cmdb-sandbox"
-    ManagedOrganizationalUnit = "CDS"
-    SSOUserEmail              = "chris@xperiencops.com"
-    SSOUserFirstName          = "CDS1"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Managed_by" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "XOPS DevOps"
-    change_reason       = "CDS single tenant account creation"
-  }
-
-  custom_fields = {
-    group = "prod"
-    vpc_cidr = "10.44"
-    vpc_name = "cmdb-sandbox-vpc-prod"
-    vpc_cidr_2 = "10.48"
-    vpc_name_2 = "cmdb-sandbox-vpc-prod-west"
-  }
-
-  account_customizations_name = "cds"
-}
-
 module "cmdb-echo" {
   source = "./modules/aft-account-request"
 
@@ -62,12 +30,12 @@ module "cmdb-echo" {
   account_customizations_name = "cds"
 }
 
-module "cds2" {
+module "cmdb-bbyit" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
     AccountEmail              = "aws-cmdb-bbyit-prod@xops.io"
-    AccountName               = "cmdbbbyit-prod"
+    AccountName               = "cmdb-bbyit-prod"
     ManagedOrganizationalUnit = "CDS"
     SSOUserEmail              = "chris@xperiencops.com"
     SSOUserFirstName          = "CDS1"
@@ -94,12 +62,12 @@ module "cds2" {
   account_customizations_name = "cds"
 }
 
-module "xops_platform" {
+module "platform_prod" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
     AccountEmail              = "aws-platform-platform-prod@xops.io"
-    AccountName               = "xplatform-prod"
+    AccountName               = "platform-prod"
     ManagedOrganizationalUnit = "Platform"
     SSOUserEmail              = "chris@xperiencops.com"
     SSOUserFirstName          = "Admin"
@@ -118,11 +86,106 @@ module "xops_platform" {
   custom_fields = {
     group = "prod"
     vpc_cidr = "10.46"
-    vpc_name = "xops-platform-vpc-prod"
+    vpc_name = "platform-prod-vpc-prod"
     vpc_cidr_2 = "10.47"
-    vpc_name_2 = "xops-platform-vpc-prod-west"
+    vpc_name_2 = "platform-prod-vpc-prod-west"
   }
 
   account_customizations_name = "platform"
 }
 
+module "platform-sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-platform-platform-sandbox@xops.io"
+    AccountName               = "platform-sandbox"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "chris@xperiencops.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Managed_by" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "XOPS DevOps"
+    change_reason       = "CDS single tenant account creation"
+  }
+
+  custom_fields = {
+    group = "prod"
+    vpc_cidr = "10.52"
+    vpc_name = "xops-platform-sandbox-vpc-prod"
+    vpc_cidr_2 = "10.53"
+    vpc_name_2 = "xops-platform-sandbox-vpc-prod-west"
+  }
+
+  account_customizations_name = "platform"
+}
+
+module "cmdb-sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-cmdb-sandbox@xops.io"
+    AccountName               = "cmdb-sandbox"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "chris@xperiencops.com"
+    SSOUserFirstName          = "CDS1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Managed_by" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "XOPS DevOps"
+    change_reason       = "CDS single tenant account creation"
+  }
+
+  custom_fields = {
+    group = "prod"
+    vpc_cidr = "10.44"
+    vpc_name = "cmdb-sandbox-vpc-prod"
+    vpc_cidr_2 = "10.48"
+    vpc_name_2 = "cmdb-sandbox-vpc-prod-west"
+  }
+
+  account_customizations_name = "cds"
+}
+
+/* module "cmdb-sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-cmdb-sandbox@xops.io"
+    AccountName               = "cmdb-sandbox2"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "chris@xperiencops.com"
+    SSOUserFirstName          = "CDS1"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Managed_by" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "XOPS DevOps"
+    change_reason       = "CDS single tenant account creation"
+  }
+
+  custom_fields = {
+    group = "prod"
+    vpc_cidr = "10.54"
+    vpc_name = "cmdb-sandbox-2-vpc-prod"
+    vpc_cidr_2 = "10.55"
+    vpc_name_2 = "cmdb-sandbox-2-vpc-prod-west"
+  }
+
+  account_customizations_name = "cds"
+} */
